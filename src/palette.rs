dc660cbcf5a2d1eeb30f818d63b99f1a2cd3705e
solojessy <solojessy.rs@gmail.com>
2024-05-08 19:57:10 +0100

    Fix GREEN RGB to actually be Green.

--- src/palette.rs
+++ src/palette.rs
@@ -4,7 +4,7 @@ use ratatui::style::Color;
 pub const WHITE: Color = Color::Rgb(247, 241, 255);
 pub const LIGHT_BLUE: Color = Color::Rgb(90, 212, 230);
 pub const RED: Color = Color::Rgb(252, 97, 141);
-pub const GREEN: Color = Color::Rgb(123, 216, 243);
+pub const GREEN: Color = Color::Rgb(123, 216, 143);
 pub const ORANGE: Color = Color::Rgb(253, 147, 83);
 pub const PURPLE: Color = Color::Rgb(148, 138, 227);
 pub const YELLOW: Color = Color::Rgb(252, 229, 102);
@@ -13,3 +13,13 @@ pub const BACKGROUND: Color = Color::Rgb(34, 34, 34);
 pub const BASE_1: Color = Color::Rgb(54, 53, 55);
 pub const BASE_2: Color = Color::Rgb(82, 80, 83);
 pub const BASE_3: Color = Color::Rgb(105, 103, 108);
+
+/*
+White:      Used for plain text
+Light Blue: Used for Key bindings
+Red:        Priority
+Green:      Priority
+Orange:     Priority
+Purple:     File Name
+Yellow:     Task Description
+*/
